5df08f81f22597b0c4523ac512e7adff16427c74
Thomas Baier <tbaier@math.bme.hu>
2022-01-06 11:26:53 +0100

    Add clean up script for Python app storage

---
+++ py-app/clean_storage.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# ASR implementation writes this files for some unknown reason
+rm *.wav
+
+rm storage/audio/*.wav
+rm storage/audio/*.json
+
+rm storage/video/*.png
+rm storage/video/*.json
